1e94b5465a7ebcdb2decf11a2e192b1bb783c45e
Peter Spierenburg <peter.spierenburg@nautel.com>
2018-02-27 12:54:22 -0400

    Added basic ui

--- src/main/groovy/org/spierepf/App.groovy
+++ src/main/groovy/org/spierepf/App.groovy
@@ -1,14 +1,73 @@
-package org.spierepf;
+package org.spierepf
 
-import groovy.swing.SwingBuilder
-import java.awt.BorderLayout as BL
+import groovy.swing.*
+import javax.swing.*
+import java.awt.*
 
-count = 0
 new SwingBuilder().edt {
-  frame(title: 'Frame', size: [300, 300], show: true) {
-    borderLayout()
-    textlabel = label(text: 'Click the button!', constraints: BL.NORTH)
-    button(text:'Click Me',
-         actionPerformed: {count++; textlabel.text = "Clicked ${count} time(s)."; println "clicked"}, constraints:BL.SOUTH)
-  }
+    lookAndFeel 'nimbus'  // Simple change in look and feel.
+    frame(title: 'App', pack: true, show: true, locationRelativeTo: null, defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE) {
+        borderLayout(vgap: 5)
+         
+        panel(constraints: BorderLayout.CENTER, border: emptyBorder(10)) {
+            tableLayout {
+                tr {
+                    td {
+                        label 'Source File:'
+                    }
+                    td {
+                        sourceFileNameTextField = textField id: 'sourceFileName', columns: 30
+                    }
+                    td {
+                        button 'Choose...', actionPerformed: {
+                            def initialPath = System.getProperty("user.dir")
+                            JFileChooser fc = new JFileChooser(initialPath)
+                            fc.setFileSelectionMode(JFileChooser.FILES_ONLY)
+                            int result = fc.showOpenDialog( null )
+                            switch ( result )
+                            {
+                            case JFileChooser.APPROVE_OPTION:
+                                sourceFileNameTextField.text = fc.getSelectedFile().getAbsolutePath()
+                                break
+                            case JFileChooser.CANCEL_OPTION:
+                            case JFileChooser.ERROR_OPTION:
+                                break
+                            }
+                        }
+                    }
+                }
+                tr {
+                    td {
+                        label 'Destination File:'
+                    }
+                    td {
+                        destinationFileNameTextField = textField id: 'destinationFileName', columns: 30
+                    }
+                    td {
+                        button 'Choose...', actionPerformed: {
+                            def initialPath = System.getProperty("user.dir")
+                            JFileChooser fc = new JFileChooser(initialPath)
+                            fc.setFileSelectionMode(JFileChooser.FILES_ONLY)
+                            int result = fc.showOpenDialog( null )
+                            switch ( result )
+                            {
+                            case JFileChooser.APPROVE_OPTION:
+                                destinationFileNameTextField.text = fc.getSelectedFile().getAbsolutePath()
+                                break
+                            case JFileChooser.CANCEL_OPTION:
+                            case JFileChooser.ERROR_OPTION:
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+             
+        }
+         
+        panel(constraints: BorderLayout.SOUTH) {
+            button text: 'OK', actionPerformed: {
+            }
+        }
+    } 
 }
