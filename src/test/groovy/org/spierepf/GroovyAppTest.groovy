02ee46f6cedd527d6be54fb49b84fb90009493db
Peter-Frank Spierenburg <spierepf@hotmail.com>
2018-02-26 23:25:17 -0400

    Added groovy test

---
+++ src/test/groovy/org/spierepf/GroovyAppTest.groovy
@@ -0,0 +1,18 @@
+package org.spierepf
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Unit test for simple App.
+ */
+public class GroovyAppTest 
+    extends GroovyTestCase
+{
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true )
+    }
+}
