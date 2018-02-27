124085efedd049006b28544d080ac8c260a274f2
Peter-Frank Spierenburg <spierepf@hotmail.com>
2018-02-27 07:29:17 -0400

    Added load

--- src/test/groovy/org/spierepf/GroovyAppTest.groovy
+++ src/test/groovy/org/spierepf/GroovyAppTest.groovy
@@ -4,6 +4,8 @@ import groovy.util.GroovyTestCase
 
 import java.io.StringReader
 import com.opencsv.CSVReader
+import java.io.StringWriter
+import com.opencsv.CSVWriter
 
 /**
  * Unit test for simple App.
@@ -28,11 +30,40 @@ public class GroovyAppTest
         return records
     }
 
+    def load(def writer, def records)
+    {
+        def csvWriter = new CSVWriter(writer)
+
+        Set headerSet = []
+        records.forEach { headerSet.addAll(it.keySet()) }
+        String[] header = new String[headerSet.size()]
+        headerSet.toArray(header)
+        csvWriter.writeNext(header)
+
+        String[] line = new String[headerSet.size()]
+        records.forEach { record ->
+            for(int i = 0; i < header.size(); ++i) {
+                line[i] = record[header[i]]
+            }
+            csvWriter.writeNext(line)
+        }
+    }
+
     public void testExtract()
     {
         CSVReader reader = new CSVReader(new StringReader('Name,ID\n"Spierenburg, Peter-Frank",12345\n'))
         def records = extract(reader)
         assertEquals('Spierenburg, Peter-Frank', records[0]['Name'])
         assertEquals('12345', records[0]['ID'])
+    }
+
+    public void testLoad()
+    {
+        def records = [[Name: 'Spierenburg, Peter-Frank', ID: 12345]]
+
+        def stringWriter = new StringWriter()
+        def writer = load(stringWriter, records)
+
+        assertEquals('"Name","ID"\n"Spierenburg, Peter-Frank","12345"\n', stringWriter.getBuffer().toString())
     }
 }
