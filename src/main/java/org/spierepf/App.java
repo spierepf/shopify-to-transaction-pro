4d440eda9294612ab57788b647551f6b3662a17c
Peter-Frank Spierenburg <spierepf@hotmail.com>
2018-02-26 20:59:06 -0400

    Initial commit

---
+++ src/main/java/org/spierepf/App.java
@@ -0,0 +1,13 @@
+package org.spierepf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
